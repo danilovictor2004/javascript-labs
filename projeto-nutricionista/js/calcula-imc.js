49e1be3721e2b8edc89d5bbc8a9319a3412e11e9
Danillo Victtor <danilo_victor2004@hotmail.com>
2020-08-12 12:33:01 -0300

    commit update

---
+++ projeto-nutricionista/js/calcula-imc.js
@@ -0,0 +1,57 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+for(var i = 0; i < pacientes.length; i++){
+	var paciente = pacientes[i];
+
+	var tdPeso = paciente.querySelector(".info-peso");
+	var tdAltura = paciente.querySelector(".info-altura");
+	var tdImc = paciente.querySelector(".info-imc");
+
+	var peso = tdPeso.textContent;
+	var altura = tdAltura.textContent;
+
+	var pesoEhValido = validaPeso(peso);
+	var alturaEhValido = validaAltura(altura);
+
+	if(!pesoEhValido){
+		tdImc.textContent = "Peso inválido!";
+		pesoEhValido = false;
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if(!alturaEhValido){
+		tdImc.textContent = "Altura inválida!";
+		alturaEhValido = false
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if(pesoEhValido && alturaEhValido){
+		var imc = calculaImc(peso, altura);
+		tdImc.textContent = imc;
+	}
+}
+
+function calculaImc(peso, altura) {
+	var imc = 0;
+	imc = peso / (altura * altura);
+	return imc.toFixed(2);
+}
+
+function validaPeso(peso){
+	if(peso > 0 && peso < 1000){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function validaAltura(altura) {
+	if(altura > 0 && altura <= 3.0){
+		return true;
+	} else{
+		return false;
+	}
+}
+
---
+++ projeto-nutricionista/js/form.js
@@ -0,0 +1,102 @@
+var btnAdiciona = document.querySelector("#adicionar-paciente");
+btnAdiciona.addEventListener("click", function () {
+	event.preventDefault();
+	
+	var form = document.querySelector("#form-adiciona");
+	var paciente = obtemPacienteDoFormulario(form);
+    
+    var erros = validaPaciente(paciente);
+    if(erros.length > 0){
+        exibeMensagensErro(erros);
+        return;
+    }
+
+    adicionaPacienteNaTabela(paciente);
+    
+    form.reset();
+
+    var erroMensagem = document.querySelector("#mensagens-erro");
+    erroMensagem.innerHTML = "";
+});
+
+function adicionaPacienteNaTabela(paciente){
+    var pacienteTr = montaTr(paciente);  
+    var tabelaPaciente = document.querySelector("#tabela-pacientes");
+    tabelaPaciente.appendChild(pacienteTr);
+}
+
+function obtemPacienteDoFormulario(form) {
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+}
+
+function montaTd(dado, classe) {
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+
+    return td;
+}
+
+function montaTr(paciente) {
+    // cria uma tr
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    // cria as td's e adiciona dentro da tr
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+}
+
+function validaPaciente(paciente) {
+    var erros = [];
+
+    if(paciente.nome.length == 0){
+        erros.push("O nome não pode ser em branco");
+    }
+
+    if(paciente.peso.length == 0){
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if(paciente.altura.length == 0){
+        erros.push("A altura não pode ser em branco");
+    }
+
+    if(paciente.gordura.length == 0){
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if(!validaPeso(paciente.peso)){
+        erros.push("O peso é inválido");
+    }
+
+    if(!validaAltura(paciente.altura)){
+        erros.push("A altura é inválida");
+    }
+
+    return erros;
+}
+
+function exibeMensagensErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(erro => {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
